f3102691ff5f9f74801808cf5f89ed4b9b8ec144
Lucas Flin <malucasnba@gmail.com>
2026-05-18 23:25:32 -0300

    feat: cadastro de personagens finalizado!

--- src/models/personagemModel.js
+++ src/models/personagemModel.js
@@ -11,15 +11,16 @@ function buscarPersonagensPorPlayer(playerId) {
 function cadastrar(playerId, nome, raca, classe, forca, destreza, constituicao, inteligencia, sabedoria, carisma, acrobacia, lidar_animais, arcanismo, atletismo, atuacao, blefar, furtividade, historia, intimidacao, intuicao, investigacao, medicina, natureza, percepcao, persuasao, prestidigitacao, religiao, sobrevivencia) {
   
   var instrucaoPersonagem = `INSERT INTO personagens (fk_player, nome, raca, classe, forc, des, cons, intel, sab, car) VALUES (${playerId}, "${nome}", "${raca}", "${classe}", ${forca}, ${destreza}, ${constituicao}, ${inteligencia}, ${sabedoria}, ${carisma})`;
-  var instrucaoPericias = `INSERT INTO pericias (fk_personagem, acrobacia, lidar_animais, arcanismo, atletismo, atuacao, blefar, furtividade, historia, intimidacao, intuicao, investigacao, medicina, natureza, percepcao, persuasao, prestidigitacao, religiao, sobrevivencia) VALUES ((select id_personagem from personagens where nome = ${nome} and fk_player = ${playerId} order by id_personagem desc limit 1), ${acrobacia}, ${lidar_animais}, ${arcanismo}, ${atletismo}, ${atuacao}, ${blefar}, ${furtividade}, ${historia}, ${intimidacao}, ${intuicao}, ${investigacao}, ${medicina}, ${natureza}, ${percepcao}, ${persuasao}, ${prestidigitacao}, ${religiao}, ${sobrevivencia})`;
+  var instrucaoPericias = `INSERT INTO pericias (fk_player, fk_personagem, acrobacia, lidar_animais, arcanismo, atletismo, atuacao, blefar, furtividade, historia, intimidacao, intuicao, investigacao, medicina, natureza, percepcao, persuasao, prestidigitacao, religiao, sobrevivencia) VALUES (${playerId}, (select id_personagem from personagens where nome = "${nome}" and fk_player = ${playerId} order by id_personagem desc limit 1), ${acrobacia}, ${lidar_animais}, ${arcanismo}, ${atletismo}, ${atuacao}, ${blefar}, ${furtividade}, ${historia}, ${intimidacao}, ${intuicao}, ${investigacao}, ${medicina}, ${natureza}, ${percepcao}, ${persuasao}, ${prestidigitacao}, ${religiao}, ${sobrevivencia})`;
   var idPersonagem = `select id_personagem from personagens where nome = ${nome} and fk_player = ${playerId} order by id_personagem desc limit 1`
 
   console.log("Executando a instrução SQL: \n" + instrucaoPersonagem);
   database.executar(instrucaoPersonagem)
   console.log("Executando a instrução SQL: \n" + instrucaoPericias);
   database.executar(instrucaoPericias)
+  console.log("Executando a instrução SQL: \n" + idPersonagem);
   var a = database.executar(idPersonagem)
-  return 
+  return a
 }
 
 
